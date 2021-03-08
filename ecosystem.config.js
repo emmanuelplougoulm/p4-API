db65a51f49dc45a6a3021b25ff36bdb2602c14b6
e.plougoulm <e.plougoulm@groupeonepoint>
2021-03-08 13:40:46 +0100

    add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: "./server.js",
+      watch: ".",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "debian",
+      host: [{ host: "193.70.115.32", port: "1025" }],
+      ref: "origin/main",
+      repo: "https://github.com/emmanuelplougoulm/p4-API",
+      path: "/home/debian/pm2",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
